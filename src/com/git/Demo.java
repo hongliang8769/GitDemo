f77925c7cbef24dbb41f9fc249764a2064b4a8b4
hongliang8769 <876950590@qq.com>
2015-09-08 20:53:10 +0800

    cc
    ccc

--- src/com/git/Demo.java
+++ src/com/git/Demo.java
@@ -3,6 +3,10 @@ package com.git;
 public class Demo {
 
 	private String msg = "git demo";
+	private String aa = "aagaaait demo";
+	private String bb = "bb git demo";
+	private String cc = "cc git demo";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         String aa = "hongliangssddd";
